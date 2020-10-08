aba8013b241d3db54e10d9f1a41046592961af3e
Maxwell Monteiro <maxmonte.vix@gmail.com>
2020-10-08 00:33:03 -0300

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,81 @@
+#############################################################################
+#Author:Maxwell
+#Date:10/07/2020
+# File: plot4.R
+#############################################################################
+
+#Installing packages
+install.packages("dplyr")
+library(dplyr)
+
+#Setting English as default config
+Sys.setlocale("LC_ALL","English")
+## Downloading the Original DataSet Electric power consumption 
+
+target<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(target,destfile="./Electric.zip",method="curl")
+
+
+#checking directory structure and unzipping the data set
+if(!file.exists("./data")){
+  dir.create("./data")
+  unzip(zipfile = "./Electric.zip",exdir ="./data" )
+}else{unzip(zipfile = "./Electric.zip",exdir ="./data" )}
+
+
+###### mapping the desired files####################
+####################################################
+
+file<-file.path("./data","household_power_consumption.txt")
+
+###### Loading the data####################
+####################################################
+data<- read.table(file,sep=";",header = TRUE)
+
+## Formatting Date from factor to string, then to Date
+data[,1]<-as.Date(data[,1],format="%d/%m/%Y") 
+
+
+
+
+## Subseting the data period:from 2007-02-01 to 2007-02-02
+
+data<-subset(data,(Date=="2007-02-01"|Date== "2007-02-02"))
+
+##Formatting columns from factor to numeric
+
+for (i in 3:9){
+  data[,i]<-as.numeric(as.character(data[,i]))
+}
+
+## Creating a new column Join_time, combining Date and Time 
+
+data$"Join_time"<-strptime(paste(data$Date,data$Time), "%Y-%m-%d %T", tz ="GMT")
+
+###### Plotting the Plot4.png####################
+####################################################
+
+png(filename = "./data/plot4.png",480,480) # opening the png device
+# Setting parameters
+par(mfrow = c(2, 2), mar = c(4, 4, 4, 4), oma = c(2, 2, 2, 2))
+
+plot(data$"Join_time",data$Global_active_power,type ="l",ylab ="Global Active Power",xlab = "" )
+plot(data$"Join_time",data$Voltage,type ="l",ylab ="Voltage",xlab = "datetime" )
+
+# plotting the base graph
+with(data,plot(data$Join_time,data$Sub_metering_1, type = "n",ylab = "Energy sub metering",xlab = ""))
+
+# plotting the Sub_metering_1 line
+with(data,points(data$Join_time,data$Sub_metering_1, type = "l",ylab = "Energy sub metering",xlab = ""))
+# plotting the Sub_metering_2 line
+with(data,points(data$Join_time,data$Sub_metering_2,col="red", type = "l",ylab = "Energy sub metering",xlab = ""))
+# plotting the Sub_metering_3 line
+with(data,points(data$Join_time,data$Sub_metering_3,col="blue", type = "l",ylab = "Energy sub metering",xlab = ""))
+
+#legend
+leg<-c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+legend("topright",lwd=1,cex =0.9,xjust=1,yjust=1,x.intersp =.075  ,text.width=100000,inset =.02,lty=1,bty="n",col=c("black","red","blue"), legend=leg )
+
+plot(data$"Join_time",data$Global_reactive_power,type ="l",ylab ="Global_reactive_power",xlab = "datetime" )
+# closing the png device
+dev.off() 
